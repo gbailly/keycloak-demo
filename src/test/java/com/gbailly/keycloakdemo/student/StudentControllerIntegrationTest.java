a5b4b6e87ef452a60f91b5f5d5e9f4b6b8d9d4c1
Guillaume Bailly <guillaume.gbailly@gmail.com>
2023-04-23 13:31:22 +0200

    Add endpoint

---
+++ src/test/java/com/gbailly/keycloakdemo/student/StudentControllerIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.gbailly.keycloakdemo.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class StudentControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getStudents() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/students"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+}
